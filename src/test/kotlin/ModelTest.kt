a5d7fdd172efd0fb7787b76625d45a5054136a17
Muhammad Hewedy <mhewedy@gmail.com>
2019-06-03 15:03:02 +0300

    testing eureka client model classes conversion into json

--- src/test/kotlin/ModelTest.kt
+++ src/test/kotlin/ModelTest.kt
@@ -1,5 +1,6 @@
 import heplers.toJson
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ModelTest {
 
@@ -19,6 +20,28 @@ class ModelTest {
             dataCenterInfo = DataCenterInfo(DcNameType.MY_OWN)
         )
 
-        println(instance.toJson())
+        val expected = """{
+              "app": "eureka-client-test",
+              "dataCenterInfo": {
+                "metadata": null,
+                "name": {
+                  "value": "MyOwn"
+                }
+              },
+              "healthCheckUrl": "http://localhost/health-check-url",
+              "homePageUrl": "http://localhost/home-page-url",
+              "hostName": "localhost",
+              "ipAddr": "192.168.1.10",
+              "leaseInfo": null,
+              "metadata": null,
+              "port": null,
+              "securePort": 0,
+              "secureVipAddress": "192.168.1.11",
+              "status": "UP",
+              "statusPageUrl": "http://localhost/status-page-url",
+              "vipAddress": "192.168.1.12"
+            }""".replace("\\s+".toRegex(), "")
+
+        assertEquals(expected, instance.toJson())
     }
 }
