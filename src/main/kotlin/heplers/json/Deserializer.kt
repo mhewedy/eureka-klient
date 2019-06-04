a8db9d8a25fd8331be574bb041b45dfd82ac5991
Muhammad Hewedy <mhewedy@gmail.com>
2019-06-04 16:50:54 +0300

    start step 3: writing simple json deserializer

---
+++ src/main/kotlin/heplers/json/Deserializer.kt
@@ -0,0 +1,13 @@
+package heplers.json
+
+object Token {
+    const val leftBracket = "["
+    const val rightBracket = "]"
+    const val leftBrace = "{"
+    const val rightBrace = "}"
+    const val doubleQuote = "\""
+}
+
+fun main() {
+    println(Token.leftBracket)
+}
