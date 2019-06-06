c5d65bd782f85ff570c770b9f8bbcc279c24d747
Muhammad Hewedy <mhewedy@gmail.com>
2019-06-06 03:26:54 +0300

    test complex objects from json.org/example.html

--- src/test/kotlin/helpers/json/ParserTest.kt
+++ src/test/kotlin/helpers/json/ParserTest.kt
@@ -2,6 +2,7 @@ package helpers.json
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 
 class ParserTest {
@@ -263,5 +264,125 @@ class ParserTest {
             ),
             actualNode
         )
+    }
+
+    @Test
+    fun `test parse complex json object from json_dot_org_slash_example_dot_html`() {
+
+        val json = """
+            {
+              "web-app": {
+                "servlet": [
+                  {
+                    "servlet-name": "cofaxCDS",
+                    "servlet-class": "org.cofax.cds.CDSServlet",
+                    "init-param": {
+                      "configGlossary:installationAt": "Philadelphia, PA",
+                      "configGlossary:adminEmail": "ksm@pobox.com",
+                      "configGlossary:poweredBy": "Cofax",
+                      "configGlossary:poweredByIcon": "/images/cofax.gif",
+                      "configGlossary:staticPath": "/content/static",
+                      "templateProcessorClass": "org.cofax.WysiwygTemplate",
+                      "templateLoaderClass": "org.cofax.FilesTemplateLoader",
+                      "templatePath": "templates",
+                      "templateOverridePath": "",
+                      "defaultListTemplate": "listTemplate.htm",
+                      "defaultFileTemplate": "articleTemplate.htm",
+                      "useJSP": "false",
+                      "jspListTemplate": "listTemplate.jsp",
+                      "jspFileTemplate": "articleTemplate.jsp",
+                      "cachePackageTagsTrack": "200",
+                      "cachePackageTagsStore": "200",
+                      "cachePackageTagsRefresh": "60",
+                      "cacheTemplatesTrack": "100",
+                      "cacheTemplatesStore": "50",
+                      "cacheTemplatesRefresh": "15",
+                      "cachePagesTrack": "200",
+                      "cachePagesStore": "100",
+                      "cachePagesRefresh": "10",
+                      "cachePagesDirtyRead": "10",
+                      "searchEngineListTemplate": "forSearchEnginesList.htm",
+                      "searchEngineFileTemplate": "forSearchEngines.htm",
+                      "searchEngineRobotsDb": "WEB-INF/robots.db",
+                      "useDataStore": "true",
+                      "dataStoreClass": "org.cofax.SqlDataStore",
+                      "redirectionClass": "org.cofax.SqlRedirection",
+                      "dataStoreName": "cofax",
+                      "dataStoreDriver": "com.microsoft.jdbc.sqlserver.SQLServerDriver",
+                      "dataStoreUrl": "jdbc:microsoft:sqlserver://LOCALHOST:1433;DatabaseName=goon",
+                      "dataStoreUser": "sa",
+                      "dataStorePassword": "dataStoreTestQuery",
+                      "dataStoreTestQuery": "SET NOCOUNT ON;select test='test';",
+                      "dataStoreLogFile": "/usr/local/tomcat/logs/datastore.log",
+                      "dataStoreInitConns": "10",
+                      "dataStoreMaxConns": "100",
+                      "dataStoreConnUsageLimit": "100",
+                      "dataStoreLogLevel": "debug",
+                      "maxUrlLength": "500"
+                    }
+                  },
+                  {
+                    "servlet-name": "cofaxEmail",
+                    "servlet-class": "org.cofax.cds.EmailServlet",
+                    "init-param": {
+                      "mailHost": "mail1",
+                      "mailHostOverride": "mail2"
+                    }
+                  },
+                  {
+                    "servlet-name": "cofaxAdmin",
+                    "servlet-class": "org.cofax.cds.AdminServlet"
+                  },
+                  {
+                    "servlet-name": "fileServlet",
+                    "servlet-class": "org.cofax.cds.FileServlet"
+                  },
+                  {
+                    "servlet-name": "cofaxTools",
+                    "servlet-class": "org.cofax.cms.CofaxToolsServlet",
+                    "init-param": {
+                      "templatePath": "toolstemplates/",
+                      "log": "1",
+                      "logLocation": "/usr/local/tomcat/logs/CofaxTools.log",
+                      "logMaxSize": "",
+                      "dataLog": "1",
+                      "dataLogLocation": "/usr/local/tomcat/logs/dataLog.log",
+                      "dataLogMaxSize": "",
+                      "removePageCache": "/content/admin/remove?cache=pages&id=",
+                      "removeTemplateCache": "/content/admin/remove?cache=templates&id=",
+                      "fileTransferFolder": "/usr/local/tomcat/webapps/content/fileTransferFolder",
+                      "lookInContext": "1",
+                      "adminGroupID": "4",
+                      "betaServer": "true"
+                    }
+                  }
+                ],
+                "servlet-mapping": {
+                  "cofaxCDS": "/",
+                  "cofaxEmail": "/cofaxutil/aemail/*",
+                  "cofaxAdmin": "/admin/*",
+                  "fileServlet": "/static/*",
+                  "cofaxTools": "/tools/*"
+                },
+                "taglib": {
+                  "taglib-uri": "cofax.tld",
+                  "taglib-location": "/WEB-INF/tlds/cofax.tld"
+                }
+              }
+            }
+        """
+
+        val actualNode = Parser(json).use {
+            it.parse()
+        } as ObjectNode
+
+        assertEquals(1, actualNode.props.size)
+        assertEquals("web-app", actualNode.props[0].first)
+        val webAppElement = actualNode.props[0].second as ObjectNode
+        assertEquals(3, webAppElement.props.size)
+        assertTrue(webAppElement.props[0].second is ArrayNode)
+        val arrayNode: ArrayNode = webAppElement.props[0].second as ArrayNode
+        val objNothing = arrayNode.elements[0] as ObjectNode
+        assertEquals(objNothing.props[1].second, "org.cofax.cds.CDSServlet")
     }
 }
